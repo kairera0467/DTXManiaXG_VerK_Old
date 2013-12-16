f21d73cd0097ee9cdb99aa8a4b83e181c987cf09
kairera0467 <kairera0467@b6a83da3-d152-4175-b845-ed239f8731af>
2013-12-16 11:13:47 +0000

    #xxxxx GITADORA表示版でCLASSIC譜面が遊べないバグを修正。

--- branches/ver236(GITADORA風)/DTXManiaプロジェクト/コード/全体/CDTXMania.cs
+++ branches/ver236(GITADORA風)/DTXManiaプロジェクト/コード/全体/CDTXMania.cs
@@ -23,7 +23,7 @@ namespace DTXMania
     {
         // プロパティ
 
-        public static readonly string VERSION = "Ver2.90GD(131215)";
+        public static readonly string VERSION = "Ver2.91GD(131216)";
         public static readonly string SLIMDXDLL = "c_net20x86_Jun2010";
         public static readonly string D3DXDLL = "d3dx9_43.dll";		// June 2010
         //public static readonly string D3DXDLL = "d3dx9_42.dll";	// February 2010
