7b386dffc0f25ae766bb046117ed0f087e9b70bb
kairera0467 <kairera0467@b6a83da3-d152-4175-b845-ed239f8731af>
2014-02-13 13:57:30 +0000

    #xxxxx アセンプリバージョンの更新。

--- trunk/DTXManiaプロジェクト/コード/全体/CDTXMania.cs
+++ trunk/DTXManiaプロジェクト/コード/全体/CDTXMania.cs
@@ -23,7 +23,7 @@ namespace DTXMania
 	{
 		// プロパティ
 
-		public static readonly string VERSION = "Ver3.00(140101)";
+		public static readonly string VERSION = "Ver3.10(140214)";
 		public static readonly string SLIMDXDLL = "c_net20x86_Jun2010";
 		public static readonly string D3DXDLL = "d3dx9_43.dll";		// June 2010
         //public static readonly string D3DXDLL = "d3dx9_42.dll";	// February 2010
