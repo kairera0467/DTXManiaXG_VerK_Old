90fc44f966821e5466f0683c47d8790ebcd36444
J.MIR <26849443+kairera0467@users.noreply.github.com>
2015-01-12 12:51:46 +0000

    #34051 ギター画面でのBPMバーを完了。 #34051 サビ区間の命令に対応。

--- DTXManiaプロジェクト/コード/ステージ/07.演奏/ドラム画面/CAct演奏DrumsBPMバー.cs
+++ DTXManiaプロジェクト/コード/ステージ/07.演奏/ドラム画面/CAct演奏DrumsBPMバー.cs
@@ -10,6 +10,13 @@ namespace DTXMania
     {
         public override int On進行描画()
         {
+            int nBPMCounter = 0;
+            if( base.ctBPMバー != null )
+            {
+                nBPMCounter = (int)base.ctBPMバー.db現在の値;
+            }
+            float dbBarPos = (float)( 6.0f * Math.Sin( Math.PI * nBPMCounter / 14.0f ) );
+
             if( base.txバー下地 != null )
                 base.txバー下地.t2D描画( CDTXMania.app.Device, 0, 0 );
             if( base.txバー穴 != null )
@@ -18,12 +25,6 @@ namespace DTXMania
                 base.txバー穴.t2D描画( CDTXMania.app.Device, 891, 44, new Rectangle( 14, 0, 14, 627 ) );
             }
 
-            int nBPMCounter = 0;
-            if( base.ctBPMバー != null )
-            {
-                nBPMCounter = (int)base.ctBPMバー.db現在の値;
-            }
-            float dbBarPos = (float)( 6.0f * Math.Sin( Math.PI * nBPMCounter / 14.0f ) );
 
             //BPMバー Height 10
             if( base.txバー本体 != null )
@@ -36,7 +37,17 @@ namespace DTXMania
 
             }
 
-            //Flush Height 32  L 48  R 80
+            //Flush Width 32  L 48  R 80
+            if( base.txバー本体 != null && base.bサビ区間中 == true )
+            {
+                base.txバーサビ.n透明度 = 255 - (int)(255 * nBPMCounter / 14);
+
+                //左
+                base.txバーサビ.t2D描画( CDTXMania.app.Device, 220 - dbBarPos, 54, new Rectangle( 48, 0, 32, 600 ) );
+
+                //右
+                base.txバーサビ.t2D描画( CDTXMania.app.Device, 896 + dbBarPos, 54, new Rectangle( 80, 0, 32, 600 ) );
+            }
 
             return base.On進行描画();
         }
