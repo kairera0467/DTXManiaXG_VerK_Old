67a26535586d80fa0934281bcecc035daa76a70f
J.MIR <26849443+kairera0467@users.noreply.github.com>
2015-09-19 04:21:28 +0000

    #xxxxx スピーカー画像の最適化、拍動の実装。

--- DTXManiaプロジェクト/コード/ステージ/07.演奏/ギター画面/CAct演奏Guitarスピーカー.cs
+++ DTXManiaプロジェクト/コード/ステージ/07.演奏/ギター画面/CAct演奏Guitarスピーカー.cs
@@ -10,10 +10,7 @@ namespace DTXMania
 {
     internal class CAct演奏Guitarスピーカー : CActivity
     {
-
-        protected CTexture スピーカー上;
-        protected CTexture スピーカー下;
-        protected CTexture txスピーカー背景;
+        protected CTexture txスピーカー;
         public CCounter ctBPM;
         public double UnitTime;
 
@@ -23,24 +20,25 @@ namespace DTXMania
             this.UnitTime = 0.0;
             base.On活性化();
         }
+        public override void On非活性化()
+        {
+            this.ctBPM = null;
+            base.On非活性化();
+        }
         public override void OnManagedリソースの作成()
         {
-            if (!base.b活性化してない)
+            if( !base.b活性化してない )
             {
-                this.スピーカー上 = CDTXMania.tテクスチャの生成(CSkin.Path(@"Graphics\7_speakerU.png"));
-                this.スピーカー下 = CDTXMania.tテクスチャの生成(CSkin.Path(@"Graphics\7_speakerD.png"));
-                this.txスピーカー背景 = CDTXMania.tテクスチャの生成(CSkin.Path(@"Graphics\7_speaker.png"));
+                this.txスピーカー = CDTXMania.tテクスチャの生成( CSkin.Path( @"Graphics\7_speaker.png" ) );
 
                 base.OnManagedリソースの作成();
             }
         }
         public override void OnManagedリソースの解放()
         {
-            if (!base.b活性化してない)
+            if( !base.b活性化してない )
             {
-                CDTXMania.tテクスチャの解放(ref this.スピーカー上);
-                CDTXMania.tテクスチャの解放(ref this.スピーカー下);
-                CDTXMania.tテクスチャの解放(ref this.txスピーカー背景);
+                CDTXMania.tテクスチャの解放( ref this.txスピーカー );
 
                 base.OnManagedリソースの解放();
             }
@@ -50,52 +48,66 @@ namespace DTXMania
 
         public override int On進行描画()
         {
-            if (!base.b活性化してない)
+            if( !base.b活性化してない )
             {
-                int num = (int)this.ctBPM.db現在の値;
+                int num = ( int )this.ctBPM.db現在の値;
 
-                float num1 = 1 + (float)( Math.Sin(Math.PI * num / 14));
+                float num1 = 1.0698f - ( float )( Math.Sin( C変換.DegreeToRadian( num ) ) / 4.0f ); //2015.09.19 kairera0467 拍動を仮実装。
 
                 float f位置X = 218;
                 float f位置Y = 231;
                 float f下差Y = 23;
 
-                if (this.スピーカー上 != null && this.スピーカー下 != null)
+                this.ctBPM.t進行LoopDb();
+
+                if( this.txスピーカー != null && this.txスピーカー != null )
                 {
-                    if (CDTXMania.DTX.bチップがある.Guitar)
+                    if( CDTXMania.DTX.bチップがある.Guitar )
                     {
-                        if (this.txスピーカー背景 != null)
-                            this.txスピーカー背景.t2D描画(CDTXMania.app.Device, 0, 0, new Rectangle(0, 0, 640, 720));
+                        if( this.txスピーカー != null )
+                        {
+                            this.txスピーカー.t2D描画( CDTXMania.app.Device, 255, 24, new Rectangle( 2, 202, 188, 11 ) );
+                            this.txスピーカー.t2D描画( CDTXMania.app.Device, 255, 223, new Rectangle( 2, 202, 188, 11 ) );
+
+                            this.txスピーカー.t2D描画( CDTXMania.app.Device, 255, 509, new Rectangle( 2, 202, 188, 11 ) );
+                            this.txスピーカー.t2D描画( CDTXMania.app.Device, 255, 708, new Rectangle( 2, 202, 188, 11 ) );
+                        }
 
                         Matrix mat1u = Matrix.Identity;
-                        //mat1u *= Matrix.Scaling( num1, num1, 1f);
-                        mat1u *= Matrix.Translation(0f - f位置X, 0f + f位置Y, 0f);
+                        mat1u *= Matrix.Scaling( num1, num1, 1f);
+                        mat1u *= Matrix.Translation( 0f - f位置X, 0f + f位置Y, 0f );
 
-                        this.スピーカー上.t3D描画(CDTXMania.app.Device, mat1u);
+                        this.txスピーカー.t3D描画( CDTXMania.app.Device, mat1u, new Rectangle( 2, 2, 188, 188 ) );
 
                         Matrix mat1d = Matrix.Identity;
-                        //mat1d *= Matrix.Scaling( num1, num1, 1f);
+                        mat1d *= Matrix.Scaling( num1, num1, 1f);
                         mat1d *= Matrix.Translation(0f - f位置X, 0f - f位置Y - f下差Y, 0f);
 
-                        this.スピーカー下.t3D描画(CDTXMania.app.Device, mat1d);
+                        this.txスピーカー.t3D描画( CDTXMania.app.Device, mat1d, new Rectangle( 2, 2, 188, 188 ) );
                     }
 
-                    if (CDTXMania.DTX.bチップがある.Bass)
+                    if( CDTXMania.DTX.bチップがある.Bass )
                     {
-                        if (this.txスピーカー背景 != null)
-                            this.txスピーカー背景.t2D描画(CDTXMania.app.Device, 640, 0, new Rectangle(640, 0, 640, 720));
+                        if( this.txスピーカー != null )
+                        {
+                            this.txスピーカー.t2D描画( CDTXMania.app.Device, 840, 24, new Rectangle( 2, 226, 188, 11 ) );
+                            this.txスピーカー.t2D描画( CDTXMania.app.Device, 840, 223, new Rectangle( 2, 226, 188, 11 ) );
+
+                            this.txスピーカー.t2D描画( CDTXMania.app.Device, 840, 509, new Rectangle( 2, 226, 188, 11 ) );
+                            this.txスピーカー.t2D描画( CDTXMania.app.Device, 840, 708, new Rectangle( 2, 226, 188, 11 ) );
+                        }
 
                         Matrix mat2u = Matrix.Identity;
-                        //mat2u *= Matrix.Scaling(num1, num1, 1f);
-                        mat2u *= Matrix.Translation(0f + f位置X, 0f + f位置Y, 0f);
+                        mat2u *= Matrix.Scaling( num1, num1, 1f );
+                        mat2u *= Matrix.Translation( 0f + f位置X, 0f + f位置Y, 0f );
 
-                        this.スピーカー上.t3D描画(CDTXMania.app.Device, mat2u);
+                        this.txスピーカー.t3D描画( CDTXMania.app.Device, mat2u, new Rectangle( 2, 2, 188, 188 ) );
 
                         Matrix mat2d = Matrix.Identity;
-                        //mat2d *= Matrix.Scaling(num1, num1, 1f);
-                        mat2d *= Matrix.Translation(0f + f位置X, 0f - f位置Y - f下差Y, 0f);
+                        mat2d *= Matrix.Scaling( num1, num1, 1f );
+                        mat2d *= Matrix.Translation( 0f + f位置X, 0f - f位置Y - f下差Y, 0f );
 
-                        this.スピーカー下.t3D描画(CDTXMania.app.Device, mat2d);
+                        this.txスピーカー.t3D描画( CDTXMania.app.Device, mat2d, new Rectangle( 2, 2, 188, 188 ) );
 
                     }
                 }
