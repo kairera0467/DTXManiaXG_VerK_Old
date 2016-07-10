ab1f4334a4c86aae4595ab0d25fa97abc1277395
J.MIR <26849443+kairera0467@users.noreply.github.com>
2016-07-10 13:20:02 +0000

    #xxxxx (4.00RC)3桁難易度入力の対応。 #xxxxx (4.00RC)一定条件でのクラッシュを回避したつもり。 #xxxxx (4.00RC)コードの一部に解放処理の追加。(ほとんど効果無し?)

--- DTXManiaプロジェクト/コード/スコア、曲/CDTX.cs
+++ DTXManiaプロジェクト/コード/スコア、曲/CDTX.cs
@@ -4014,7 +4014,13 @@ namespace DTXMania
 					int dlevel;
 					if( int.TryParse( strパラメータ, out dlevel ) )
 					{
-						this.LEVEL.Drums = Math.Min( Math.Max( dlevel, 0 ), 100 );	// 0～100 に丸める
+						this.LEVEL.Drums = Math.Min( Math.Max( dlevel, 0 ), 1000 );	// 0～100 に丸める
+                        if( this.LEVEL.Drums >= 100 )
+                        {
+                            int dlevelTemp = this.LEVEL.Drums;
+                            this.LEVEL.Drums = (int)( this.LEVEL.Drums / 10.0f );
+                            this.LEVELDEC.Drums = dlevelTemp - this.LEVEL.Drums * 10;
+                        }
 					}
 				}
 				//-----------------
@@ -4028,7 +4034,13 @@ namespace DTXMania
 					int glevel;
 					if( int.TryParse( strパラメータ, out glevel ) )
 					{
-						this.LEVEL.Guitar = Math.Min( Math.Max( glevel, 0 ), 100 );		// 0～100 に丸める
+						this.LEVEL.Guitar = Math.Min( Math.Max( glevel, 0 ), 1000 );		// 0～100 に丸める
+                        if( this.LEVEL.Guitar >= 100 )
+                        {
+                            int glevelTemp = this.LEVEL.Guitar;
+                            this.LEVEL.Guitar = (int)( this.LEVEL.Guitar / 10.0f );
+                            this.LEVELDEC.Guitar = glevelTemp - this.LEVEL.Guitar * 10;
+                        }
 					}
 				}
 				//-----------------
@@ -4042,7 +4054,13 @@ namespace DTXMania
 					int blevel;
 					if( int.TryParse( strパラメータ, out blevel ) )
 					{
-						this.LEVEL.Bass = Math.Min( Math.Max( blevel, 0 ), 100 );		// 0～100 に丸める
+						this.LEVEL.Bass = Math.Min( Math.Max( blevel, 0 ), 1000 );		// 0～100 に丸める
+                        if( this.LEVEL.Bass >= 100 )
+                        {
+                            int blevelTemp = this.LEVEL.Bass;
+                            this.LEVEL.Bass = (int)( this.LEVEL.Bass / 10.0f );
+                            this.LEVELDEC.Bass = blevelTemp - this.LEVEL.Bass * 10;
+                        }
 					}
 				}
 				//-----------------
