746238938f0a11461a189ef09025e8d7e12727cf
kairera0467 <gennsou76573@gmail.com>
2023-11-15 21:26:21 +0900

    不要なコードの削除

--- DTXManiaプロジェクト/コード/全体/CDTXMania.cs
+++ DTXManiaプロジェクト/コード/全体/CDTXMania.cs
@@ -39,11 +39,6 @@ namespace DTXMania
             get;
             protected set;
         }
-        public static CApp App
-        {
-            get;
-            private set;
-        }
 		public static C文字コンソール act文字コンソール
 		{ 
 			get;
@@ -1682,7 +1677,7 @@ for (int i = 0; i < 3; i++) {
         {
             try
             {
-                return new CTexture(CDTXMania.App.D3D9Device, width, height, CDTXMania.TextureFormat);
+                return new CTexture(CDTXMania.app.Device, width, height, CDTXMania.TextureFormat);
             }
             catch
             {
